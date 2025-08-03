4f54aa0767d0630572e46e936d29497743e66fcd
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-08-02 22:33:57 -0600

    Crea rutas para el inicio de sesión

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Login from './pages/Login';
+import AdminPage from './pages/AdminPage';
+import Prestamista from './pages/Prestamista';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    
   );
 }
 
