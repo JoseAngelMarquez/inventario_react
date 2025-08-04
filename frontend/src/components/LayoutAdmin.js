be480526ba7a66783e271b516f14b44fcd203788
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-08-03 21:38:58 -0600

    Usa componente cerrarsesion

--- frontend/src/components/LayoutAdmin.js
+++ frontend/src/components/LayoutAdmin.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { Link, Outlet, useNavigate} from "react-router-dom";
+import CerrarSesion from "./CerrarSesión";
 import "./LayoutAdmin.css"; // Puedes definir estilos aquí
 
 const LayoutAdmin = () => {
-  const navigate = useNavigate();
-
-  const cerrarSesion = () =>{
-    localStorage.removeItem("usuario");
-    navigate("/");
-  };
+ 
   return (
     <div className="layout">
       <aside className="sidebar">
@@ -19,11 +15,7 @@ const LayoutAdmin = () => {
             <li><Link to="/admin/usuarios">Agregar Usuarios</Link></li>
             <li><Link to="/admin/prestamos">Préstamos</Link></li>
             <li><Link to="/admin/materiales">Materiales</Link></li>
-            <li>
-              <button onClick={cerrarSesion} style={{ marginTop: "1rem" }}>
-                Cerrar sesión
-              </button>
-            </li>
+            <li><CerrarSesion /></li>
           </ul>
         </nav>
       </aside>
--- frontend/src/components/LayoutPrestamista.js
+++ frontend/src/components/LayoutPrestamista.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { Link, Outlet, useNavigate } from "react-router-dom";
-
+import { Link, Outlet } from "react-router-dom";
+import CerrarSesion from "./CerrarSesión";
 const LayoutPrestamista = () => {
-  const navigate = useNavigate();
-
-  const cerrarSesion = () =>{
-    localStorage.removeItem("usuario");
-    navigate("/");
-  };
+  
   return (
     <div className="layout">
       <aside className="sidebar">
@@ -17,11 +12,7 @@ const LayoutPrestamista = () => {
             <li><Link to="/prestamista">Inicio</Link></li>
             <li><Link to="/prestamista/prestamos">Préstamos</Link></li>
             <li><Link to="/prestamista/materiales">Materiales</Link></li>
-            <li>
-              <button onClick={cerrarSesion} style={{ marginTop: "1rem" }}>
-                Cerrar sesión
-              </button>
-            </li>
+            <li><CerrarSesion /></li>
           </ul>
         </nav>
       </aside>
