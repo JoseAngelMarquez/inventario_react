2884213102ea2009bbb0eddec6deea3e74a3bb36
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-08-09 20:32:52 -0600

    Crea el servicio de inventario

--- frontend/src/services/inventarioService.js
+++ frontend/src/services/inventarioService.js
@@ -1,3 +1,5 @@
-import axios  from "axios";
+import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL+'/inventario/materiales'
+const API_URL = process.env.REACT_APP_API_URL + '/inventario/materiales';
+
+export const obtenerMateriales = () => axios.get(API_URL);
