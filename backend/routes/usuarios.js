cf9cdf1c3abd6644116dc190c24fb40882f7d11a
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-08-20 09:39:56 -0600

    Agrega la ruta de eliminar a usuarios

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -5,4 +5,5 @@ const usuarioController = require('../controllers/usuarioController');
 router.post('/login', usuarioController.login);
 router.post('/crear', usuarioController.crearUsuario);
 router.get('/', usuarioController.obtenerUsuarios);
+router.delete('/:id', usuarioController.eliminarUsuario);
 module.exports = router;
