a2b8254243ee9bf89bef43d22b9c4deb9304e991
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-08-27 14:14:09 -0600

    Agrega la función del filtro

--- backend/routes/materialRoutes.js
+++ backend/routes/materialRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const materialController = require('../controllers/materialController');
+router.get("/filtro", materialController.filtrarMaterialPorNombre);
 
 router.get('/', materialController.obtenerMateriales);
 router.get('/:id', materialController.obtenerMaterialPorId);
