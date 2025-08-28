ac6a855e9a0d6f2bf2f306f75d565cdea9d7dc80
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-08-28 12:29:10 -0600

    Añade la ruta a préstamos

--- backend/routes/prestamosRoutes.js
+++ backend/routes/prestamosRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/prestamosController');
-
+router.get('/filtro',controller.filtrarPrestamos)
 
 router.get('/', controller.obtenerTodos);
 router.get('/:id', controller.obtenerPorId);
